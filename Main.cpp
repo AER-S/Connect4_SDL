962016f497640f94b7fa4d5da0dc0ac645f81553
AER-S <89151307+AER-S@users.noreply.github.com>
2022-01-28 15:36:46 +0100

    Creating the window.

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <SDL.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	std::cout << "Hello World!" << std::endl;
+	SDL_Window* window = SDL_CreateWindow("Connect 4", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+	SDL_Delay(5000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	//std::cout << "Hello World!" << std::endl;
 	return 0;
 }
